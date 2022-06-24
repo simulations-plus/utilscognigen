8737b7b48ca5d17863f8bb075a5e4e89a1e3dcd3
ndelcamp <nash.delcamp@cognigencorp.com>
2022-06-23 23:53:00 -0400

    let file_open show directories in RStudio

--- R/file_open.R
+++ R/file_open.R
@@ -1,9 +1,13 @@
-#' Open files according to their extensions
+#' Open files according to their extensions or show a directory
 #'
-#' The files will be opened if possible. The behavior for files that cannot be
-#' displayed in the RStudio source editor or a browser is determined by
-#' \code{no_copies}. When \code{FALSE} (the default), a read-only copy will be
-#' opened. When \code{TRUE}, the Windows file path is printed as a message.
+#' @description 
+#' The files will be opened if possible. Up to one directory can be shown in the
+#' RStudio Files pane. 
+#' 
+#' The behavior for files that cannot be displayed in the RStudio source editor
+#' or a browser is determined by \code{no_copies}. When \code{FALSE} (the
+#' default), a read-only copy will be opened. When \code{TRUE}, the Windows file
+#' path is printed as a message.
 #'
 #' @param ... file paths to be opened
 #' @param no_copies \code{logical} indicating whether to avoid downloading
@@ -14,23 +18,47 @@
 #' @export
 file_open <- function(..., no_copies = FALSE) {
 
-  paths <- unlist(list(...))
+  paths <- unique(unlist(list(...)))
 
   assertthat::assert_that(
     is.character(paths),
-    is.logical(no_copies)
+    is.logical(no_copies),
+    testthat::is_testing() || interactive()
     )
+  
+  dir_paths <- paths[dir.exists(paths)]
+  
+  if(length(dir_paths) > 1) {
+    cli::cli_warn(
+      c(
+        "Only one directory can be included in {.fn file_open}.",
+        set_all_names(dir_paths, "!")
+      )
+    )
+    show_dirs <- FALSE
+  } else if(length(dir_paths) == 1 && !rstudioapi::isAvailable()) {
+    cli::cli_warn(
+      c(
+        "RStudio is not available. Cannot show directory:",
+        set_all_names(dir_paths, "!")
+      )
+    )
+    show_dirs <- FALSE
+  } else {
+    show_dirs <- TRUE
+  }
 
   vapply(X = paths,
          FUN = .file_open,
          FUN.VALUE = logical(1),
-         no_copies = no_copies
+         no_copies = no_copies,
+         show_dirs = show_dirs
          )
 
 }
 
 # Open a single file
-.file_open <- function(path, no_copies = FALSE) {
+.file_open <- function(path, no_copies = FALSE, show_dirs) {
 
   if(!file.exists(path)) {
     cli::cli_alert_danger("File not found: {.file {path}}")
@@ -41,15 +69,22 @@ file_open <- function(..., no_copies = FALSE) {
     is.character(path),
     length(path) == 1,
     is.logical(no_copies),
+    is.logical(show_dirs),
+    length(show_dirs) == 1,
     .Platform$OS.type == "unix"
     )
 
-  if(dir.exists(path)) {
-    cli::cli_alert_danger("File is a directory: {.file {path}}")
+  if(dir.exists(path) && show_dirs) {
+    cli::cli_alert_info("Showing directory: {.file {path}}")
+    rstudioapi::filesPaneNavigate(path)
+    return(TRUE)
+  } else if(dir.exists(path) && !show_dirs) {
+    # not showing directories.
+    # warning has already been output by file_open with all directories.
     return(FALSE)
   }
 
-  path <- normalizePath(path)
+  path <- fs::path_real(path)
 
   file_ext <- tools::file_ext(path)
 
